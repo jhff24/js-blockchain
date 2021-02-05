809f62b51725bac96479438b9e9fbf545c5e887b
jhff24 <johnhenry.fitzgerald@colorado.edu>
2021-02-05 13:23:04 -0500

    more testing

--- dev/test.js
+++ dev/test.js
@@ -3,7 +3,9 @@ const Blockchain = require('./blockchain.js');
 const gitcoin = new Blockchain();
 
 gitcoin.createNewBlock(1234, 'ALKJSDF2384', 'AHASFDHGASDFHG87');
-gitcoin.createNewBlock(9000, 'HHJSFJGHDBJH', 'ASDFGERGFG');
-gitcoin.createNewBlock(50247, 'CASDCSDCASDC', 'KLHERWLKJVNLDKJF89');
 
-console.log(gitcoin);
+gitcoin.createNewTransaction(100, 'ALEXFAJHSDFL', 'JENNTYVJRKJHB');
+
+gitcoin.createNewBlock(12367, 'QPWIEYROQIW', 'F9823098JR4F');
+
+console.log(gitcoin.chain[1]);
