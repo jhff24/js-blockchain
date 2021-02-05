ac3dff6b8b97a8d4f11f7c9d214c9c87aecfd0f5
jhff24 <johnhenry.fitzgerald@colorado.edu>
2021-02-05 12:29:58 -0500

    test

---
+++ dev/blockchain.js
@@ -0,0 +1,8 @@
+
+
+//Blockchain constructor function
+function Blockchain(){
+  this.chain = [];//all created/mined blocks stored here
+  this.newTransactions = [];//hold all new transactions before they are placed into a block and mined
+  
+}
