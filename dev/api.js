2ad0c5fc17afdfca223e1910cb6e8e4ec14d87b4
jhff24 <johnhenry.fitzgerald@colorado.edu>
2021-02-09 13:57:56 -0500

    blockchain get

--- dev/api.js
+++ dev/api.js
@@ -2,18 +2,28 @@
 //Date:2/7/2021
 //John Henry Fitzgerald
 
+
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const gitcoin = new Blockchain();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 //endpoint 1
 //send back entire blockchain when hit
 app.get('/blockchain', function (req, res) {
-
+  res.send(gitcoin);
 });
 
 //create new transaction when hit
 app.post('/transaction', function(req, res){
-
+  console.log(req.body);
+  res.send(`The amount  of the transaction is ${req.body.amount} gitcoin.`);
 });
 
 //create a new block when hit
